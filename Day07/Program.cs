a0ab4ced09589e223d1b1bf4d9d241bde6b328a3
Tóth Emese <tothemese.epszerk@gmail.com>
2016-12-11 14:18:18 +0100

    Day 07 solved

---
+++ Day07/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PART 01
+            string[] input = File.ReadAllLines(@"..\..\Day07-input.txt");
+            int validCounter = 0;
+            int validSSLCounter = 0;
+
+            foreach (string x in input)
+            {
+                List<string> outside = new List<string>();
+                List<string> inside = new List<string>();
+
+                Splitter(x, inside, outside);
+
+                if (IsValidTLS(inside, outside))
+                    validCounter++;
+
+                if (IsValidSSL(inside, outside, x))
+                    validSSLCounter++;
+            }
+
+            Console.WriteLine("Valid - PART 1: {0}", validCounter);
+            Console.WriteLine("Valid - PART 2: {0}", validSSLCounter);
+            Console.ReadKey();
+        }
+
+
+        public static bool IsABBA(string text)
+        {
+            for (int i = 0; i < text.Length - 3; i++)
+            {
+                string actual = text.Substring(i, 4);
+                if (actual[0] == actual[3] && actual[1] == actual[2] && actual[0] != actual[1])
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool IsValidTLS(List<string> inside, List<string> outside)
+        {
+            bool isABBAInside = false;
+            bool isABBAOutside = false;
+            foreach (string y in inside)
+            {
+                if (IsABBA(y))
+                    isABBAInside = true;
+            }
+
+            foreach (string y in outside)
+            {
+                if (IsABBA(y))
+                    isABBAOutside = true;
+            }
+
+            return (isABBAOutside && !isABBAInside);
+        }
+
+        //PART 02 
+
+        public static bool IsValidSSL(List<string> inside, List<string> outside, string input)
+        {
+            List<string> aba = new List<string>();
+            foreach (string x in outside)
+            {
+                for (int i = 0; i < x.Length - 2; i++)
+                {
+                    string actual = x.Substring(i, 3);
+                    if (actual[0] == actual[2] && actual[0] != actual[1])
+                        aba.Add(actual);
+                }
+            }
+
+            //if (aba.Count > 1)
+            //{
+            //    Console.WriteLine("\n\rLine: {0}\n\rABA: {1}",input, string.Join(",", aba));
+            //}
+
+            foreach (string z in aba)
+            {
+                string bab = z[1].ToString() + z[0].ToString() + z[1].ToString();
+
+                foreach (string y in inside)
+                    if (y.Contains(bab))
+                        return true;
+            }
+            //Console.WriteLine("\n\rLine: {0}\n\rABA: {1}",input, string.Join(",",aba));
+           
+            return false;
+        }
+
+        public static void Splitter(string x, List<string> inside, List<string> outside)
+        {
+            string actual = "";
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                switch (x[i])
+                {
+                    case '[':
+                        outside.Add(actual);
+                        actual = "";
+                        break;
+                    case ']':
+                        inside.Add(actual);
+                        actual = "";
+                        break;
+                    default:
+                        actual += x[i];
+                        if (i == x.Length - 1)
+                            outside.Add(actual);
+                        break;
+                }
+            }
+        }
+
+    } //class Program
+} //namespace
+
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Day07Test()
+        {
+            foreach (var line in new[] {
+                //new KeyValuePair<string, bool> ("aba[bab]xyz", true),
+                //new KeyValuePair<string, bool> ("xyx[xyx]xyx", false),
+                //new KeyValuePair<string, bool> ("aaa[kek]eke", true),
+                //new KeyValuePair<string, bool> ("zazbz[bzb]cdb", true),
+                new KeyValuePair<string, bool> ("sdeqalvkcrkrinxry[xezvqhybsiwnncuafq]wgmtvsnsbilzxdd[exabnsnnyiyrxkdd]wmtluopjkaunmyyogc[mcecujbnhewtxxny]fasqbsmncghkmvv", true ),
+            //    new KeyValuePair<string, bool> ("wqkcjtsnsnrnoguze[qtkujvopoiwnsnyj]wjpnbkzsrkdmjwhk[myorbznqrnieutxbt]bivalvvdqsjssmgoin", true ),
+            //    new KeyValuePair<string, bool> ("jcghfctceivcaiweue[eftngalnwvhjjsmznr]fawobojxajdxwqqzs[ooswxgrpxwyohdyruo]nmtyadyigbjerrgww[kawttctyrgxigajaicc]hmwgzbdzmeoyths", true ),
+            //    new KeyValuePair<string, bool> ("cwvxqqouchaqwkhpcfz[elwmjtglbrbyxnyoyyd]nccylfdoyorjbdi", true )
+            })
+            {
+                List<string> inside = new List<string>();
+                List<string> outside = new List<string>();
+
+                Day07.Program.Splitter(line.Key, inside, outside);
+                bool actualResult = Day07.Program.IsValidSSL(inside, outside, line.Key);
+                Assert.AreEqual(line.Value, actualResult);
+            }
+
+            
+
+        }
+    }
+}
