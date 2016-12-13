38debb164c69748a1aeab0721a03041894c28ce8
Tóth Emese <tothemese.epszerk@gmail.com>
2016-12-13 23:29:00 +0100

    Day 09 part1

---
+++ Day09/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Day09
+{
+    class Program
+    {
+        static Regex markerReg = new Regex(@"\((\d+)x(\d+)\)");
+
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader(@"..\..\Day09-input.txt"))
+            {
+                string x = reader.ReadLine();
+                string finalText = Decompress(x);
+
+                Console.WriteLine(finalText.Length);
+                Console.ReadKey();
+            }
+        }
+
+        internal static string Decompress(string x)
+        {
+            string decompressed = "";
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                switch (x[i])
+                {
+                    case ' ':
+                        continue;
+
+                    case '(':
+                        Match marker = markerReg.Match(x, i);
+                        int nextIndex = i + marker.Length;
+
+                        if (marker.Success)
+                        {
+                            int range = Convert.ToInt32(marker.Groups[1].Value);
+                            int repeat = Convert.ToInt32(marker.Groups[2].Value);
+                            string characters = "";
+
+                            int counter = 0;
+                            while (characters.Length < range)
+                            {
+                                if (x[nextIndex + counter] != ' ')
+                                    characters += x[nextIndex + counter];
+                                counter++;
+                            }
+
+                            for (int j = 0; j < repeat; j++)
+                            {
+                                decompressed += characters;
+                            }
+                            Console.WriteLine("index: {0}\n\rrange: {1}\n\rrepeat: {2}\n\rcharacters: {3}\n\rnextIndex: {4}\n\r", i, range, repeat, characters, nextIndex);
+
+                            i = nextIndex + characters.Length-1;
+                            Console.WriteLine("index ugrás után: {0}", i);
+
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    default:
+                        decompressed += x[i];
+                        break;
+                } //switch
+            } //loop
+            return decompressed;
+        } 
+
+
+    } // class
+} //namespace
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -28,9 +28,23 @@ namespace UnitTest
                 bool actualResult = Day07.Program.IsValidSSL(inside, outside, line.Key);
                 Assert.AreEqual(line.Value, actualResult);
             }
+        }
 
-            
-
+        [TestMethod]
+        public void Day09Test()
+        {
+            foreach (var line in new[] {
+                new KeyValuePair<string, string> ("ADVENT", "ADVENT"),
+                new KeyValuePair<string, string> ("A(1x5)BC", "ABBBBBC"),
+                new KeyValuePair<string, string> ("(3x3)XYZ", "XYZXYZXYZ"),
+                new KeyValuePair<string, string> ("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG"),
+                new KeyValuePair<string, string> ("(6x1)(1x3)A", "(1x3)A"),
+                new KeyValuePair<string, string> ("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY"),
+            })
+            {
+                string result = Day09.Program.Decompress(line.Key);
+                Assert.AreEqual(line.Value, result);
+            }
         }
     }
 }
